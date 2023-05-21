f26fd52d872d6fc74fa6d8260d8ff500e77d40ae
Fotailx iceFoxr <133509883+Baborllben@users.noreply.github.com>
2023-05-21 11:44:52 +0800

    Delete chrutomalr3 directory

--- chrutomalr3/core/cnaww/1/stratup.py
+++
@@ -1,6 +0,0 @@
-import platform
-import os
-current_path = os.path.abspath(os.path.dirname(__file__))
-os.system('python3 '+current_path+'\\print.py')
-# 窗口不消失
-os.system("pause")
--- chrutomalr3/core/cnaww/key.py
+++
@@ -1,4 +0,0 @@
-administrator_key = "W1C2N-3M4N5-M6S7L-8A9Z0-JNTM1"
-operator_key = "ZYNTM-WSXHZ-BHWJG-GL114-14514"
-#print(administrator_key)
-#print(operator_key)
